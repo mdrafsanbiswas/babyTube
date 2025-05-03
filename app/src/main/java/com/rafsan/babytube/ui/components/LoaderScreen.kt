e9c296ee6ee68665ba01b3082e48136915b41e5c
mdrafsanbiswas <rafsan@wind.com>
2025-05-03 22:04:12 +0600

    Make loader alignment center

--- app/src/main/java/com/rafsan/babytube/ui/components/LoaderScreen.kt
+++ app/src/main/java/com/rafsan/babytube/ui/components/LoaderScreen.kt
@@ -4,11 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LoaderScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
-        CircularProgressIndicator()
+        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
     }
 }
